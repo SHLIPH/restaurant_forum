ffbd13fa063fbe52efda265a622da6dc527f8c9a
Bingxian <wontstoplearn@gmail.com>
2017-11-25 01:46:41 +0800

    set up admin authentication

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,4 +1,7 @@
 class Admin::RestaurantsController < ApplicationController
+  before_action :authenticate_user! #Devise 提供的預設方法，檢查使用者是否登入
+  before_action :authenticate_admin #等下我們要自己手工定義的方法，檢查使用者身份是否為「管理者」。
+
   def index
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+  def authenticate_admin
+    #current_user 是 Devise 提供的 helper method。
+    #先透過 current_user 拿到 User instance，接著呼叫一個叫 admin? 的 instance method。
+    #admin? 方法是等下我們要手工加入的，這個方法會去驗證 User instance 是否具有管理者身份。從命名方式，可以判斷出 admin? 會回傳 True 或 False。
+    unless current_user.admin?
+      flash[:alert] = "Not allow!"
+      redirect_to root_path
+    end
+  end
 end
