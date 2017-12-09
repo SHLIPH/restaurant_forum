de2933e7ba2b52c61dda714a1d7c41f0589b7898
SHLIPH <wontstoplearn@gmail.com>
2017-12-09 17:21:02 +0800

    move authenticate_user! to application_controller.rb

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CategoriesController < ApplicationController
-  before_action :authenticate_user!
+  
   before_action :authenticate_admin
   before_action :set_category, only: [:update, :destroy]
 
@@ -37,7 +37,7 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def destroy
-    
+
     @category.destroy
     flash[:alert] = "category was successfully deleted"
     redirect_to admin_categories_path
