48b15f4c03876303cfaca59f771a3b7a8329533a
SHLIPH <wontstoplearn@gmail.com>
2017-12-27 17:28:00 +0800

    upload avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  mount_uploader :avatar, PhotoUploader
+
   def admin?
     self.role == "admin"
   end
