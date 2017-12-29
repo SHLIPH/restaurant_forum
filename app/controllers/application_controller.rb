43996b2171e16440b421f01dead4204817ef6b55
SHLIPH <wontstoplearn@gmail.com>
2017-12-29 10:33:53 +0800

    customize Devise, add user name to registration form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user! #Devise 提供的預設方法，檢查使用者是否登入
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
 end
