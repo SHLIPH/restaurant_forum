c6b3c1f1788ddbd1445f504979ee57a20628bec3
SHLIPH <wontstoplearn@gmail.com>
2018-01-04 14:29:22 +0800

    setup relation among user, restaurant and favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,10 @@ class User < ApplicationRecord
 
   mount_uploader :avatar, PhotoUploader
 
+  # 「使用者收藏很多餐廳」的多對多關聯
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_restaurants, through: :favorites, source: :restaurant
+  
   def admin?
     self.role == "admin"
   end
