e9fa21512de43c294fb142107760fd6086d12254
SHLIPH <wontstoplearn@gmail.com>
2018-01-19 15:22:46 +0800

    L3: add inverse_friends and friends relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,9 @@ class User < ApplicationRecord
 
   has_many :friendships, dependent: :destroy
   has_many :friendings, through: :friendships
-  #has_many :friends, class_name: "Friendship", foreign_key: "friending_id"
+
+  has_many :inverse_friends, class_name: "Friendship", foreign_key: "friending_id"
+  has_many :friends, through: :inverse_friends, source: :user
 
   def admin?
     self.role == "admin"
